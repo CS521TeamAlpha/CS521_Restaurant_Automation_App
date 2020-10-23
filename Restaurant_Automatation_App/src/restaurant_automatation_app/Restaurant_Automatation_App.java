9593ec625cc4063502a2862f81651bea16bcee73
Vaibhav T <Vaibhav T@LAPTOP-LCQ0NIGC>
2020-10-23 15:28:45 -0500

    vaibhav 1st commit

--- Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
+++ Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
@@ -18,6 +18,23 @@ public class Restaurant_Automatation_App {
         System.out.println("hello world");
         //johnny wuz here
         //keith wuz here too!!!
-    }
+        System.out.println("Awww yeah!\n" +
+        "You gotta get schwifty!\n" +
+        "You gotta get schwifty in here!\n" +
+        "It's time to get schwifty!\n" +
+        "Oh, oh\n" +
+        "You gotta get schwifty!\n" +
+        "Oh yeah!\n" +
+        "Take off your pants and your panties!\n" +
+        "Shit on the floor!\n" +
+        "Time to get schwifty in here!\n" +
+        "Gotta shit on the floor\n" +
+        "I'm Mr Bulldops!\n" +
+        "I'm Mr Bulldops\n" +
+        "take a shit on the floor!\n" +
+        "Take off your panties and your pants\n" +
+        "Time to get schwifty in here!\n" +
+        "HEY TAKE YOUR PANTS OFF ITS SCHWIFTY TIME TODAY!!!");
+    }////vvvvvv
     
 }
