987245e6527e2b09a931c11d834c649657af85ec
Johnny <Johnny@DESKTOP-UB9BIJN>
2020-10-23 15:22:40 -0500

    johnny's initial commit

--- Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
+++ Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
@@ -16,6 +16,7 @@ public class Restaurant_Automatation_App {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        //johnny wuz here
     }
     
 }
