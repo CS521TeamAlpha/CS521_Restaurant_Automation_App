2a67b7534a2ecc7963dbc8e7bab4b5038d165395
Owner <Owner@DESKTOP-3MIKOVP.mshome.net>
2020-10-23 15:05:59 -0500

    Test2

--- Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
+++ Restaurant_Automatation_App/src/restaurant_automatation_app/Restaurant_Automatation_App.java
@@ -15,7 +15,7 @@ public class Restaurant_Automatation_App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello world");
     }
     
 }
