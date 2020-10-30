8544f022a7f39467294fd53f5b76615072d19ef0
Johnny <Johnny@DESKTOP-UB9BIJN>
2020-10-30 14:44:26 -0500

    Johnny: removed infinite login pad bug

--- Restaurant_Automatation_App/src/restaurant_automatation_app/MainClass.java
+++ Restaurant_Automatation_App/src/restaurant_automatation_app/MainClass.java
@@ -17,7 +17,7 @@ import GUI.LoginScreen;
  */
 public class MainClass {
     public static void main (String [] args){
-          java.awt.EventQueue.invokeLater(new Runnable(){
+        java.awt.EventQueue.invokeLater(new Runnable(){
         public void run(){
             new MainImage().setVisible(true);
         }
